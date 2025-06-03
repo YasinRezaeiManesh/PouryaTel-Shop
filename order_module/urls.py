5b9362cd016922475922be01086e0ffb26d56a62
alfa <logicalfa.mail@gmail.com>
2025-06-03 11:19:40 +0330

    create zarinpal urls

--- order_module/urls.py
+++ order_module/urls.py
@@ -4,4 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('add-to-order/', add_to_order, name='add_to_order'),
+    path('request-payment/', request_payment, name='request_payment'),
+    path('verify-payment/', verify_payment, name='verify_payment'),
 ]

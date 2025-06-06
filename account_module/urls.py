44729ebb388d3ffaf6519966e0dd8d2d281a425c
alfa <logicalfa.mail@gmail.com>
2025-06-06 16:36:21 +0330

    create forgot_pass url

--- account_module/urls.py
+++ account_module/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('forgot-pass/', ForgotPasswordView.as_view(), name='forgot_pass'),
 ]
